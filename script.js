413571909adffba605f0b535b92ea350991abd5f
Gabriel M. Teixeira <gabadev48@protonmail.com>
2023-06-06 01:36:46 -0300

    alguns ajustes
    Ainda fico um pouco perdido para utilizar o LazyGit, mas estou explorando mais...

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function login() {
-  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
   // Perform your login logic here
 
   // For now, we'll just display the entered username and password
-  alert("Username: " + username + "\nPassword: " + password);
+  alert("E-mail: " + email + "\nPassword: " + password);
 
   // You can redirect the user to another page after successful login
   // window.location.href = "dashboard.html";
